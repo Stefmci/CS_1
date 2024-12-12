2729bed03a5ef6579a5e5c4f65aff966158a7c79
Stefmci <hs7335@mci4me.at>
2024-12-12 12:06:43 +0100

    Code hinzugefügt in der main

--- main.py
+++ main.py
@@ -13,5 +13,21 @@ current_device = st.selectbox(label='Gerät auswählen',
 
 st.write(F"Das ausgewählte Gerät ist {current_device}")
 
+# Callbacks for the number inputs
+def update_price_from_euro():
+    st.session_state.price_dollar = st.session_state.price_euro * 1.1
+
+def update_price_from_dollar():
+    st.session_state.price_euro = st.session_state.price_dollar * 0.9
+
+# Number inputs for the maintenance costs
+st.number_input(label="Wartungskosten in Euro", 
+                            key = "price_euro",
+                            on_change=update_price_from_euro)
+
+st.number_input(label="Wartungskosten in Dollar", 
+                            key = "price_dollar",
+                            on_change=update_price_from_dollar)
+
 #streamlit run main.py
 #streamlit python -m streamlit run main.py
